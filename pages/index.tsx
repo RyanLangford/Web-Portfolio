0c6ff90d975a8afcd32589437d6e55e4d2e8906c
Ryan Langford <ryanclangford@icloud.com>
2023-03-26 12:35:05 -0700

    fixed build errors, testing double click bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,31 @@
 import Head from 'next/head';
 
 export default function Home() {
+  if (typeof window === "object") {
+    // code is running in a browser environment
+    console.log("browser")
+    const slidesContainer = document.getElementById("slides-container");
+    const slide = document.querySelector(".slide");
+    const prevButton = document.getElementById("slide-arrow-prev");
+    const nextButton = document.getElementById("slide-arrow-next");
+
+    nextButton.addEventListener("click", () => {
+      console.log("slide next");
+      const slideWidth = (slide.clientWidth);
+      console.log('scrolling ' + slideWidth + ' pixels right')
+      slidesContainer.scrollLeft += slideWidth;
+    });
+    prevButton.addEventListener("click", () => {
+      console.log("slide prev");
+      const slideWidth = (slide.clientWidth);
+      console.log('scrolling ' + slideWidth + ' pixels left')
+      slidesContainer.scrollLeft -= slideWidth;
+    });
+  } else {
+    // code is running in a non-browser environment
+    console.log("non-browser")
+  }
+
   return (
     <div className="page">
       <Head>
@@ -178,7 +203,6 @@ export default function Home() {
           Source code on{' '}
           <img src="/github.png" alt="Github" className="logo" />
         </a>
-        <script type="text/javascript" src="index.js"></script>
       </footer>
     </div>
   )
